119b9107bf0d0c815a6f8b8a90841e0984228172
hibasalem <salemhiba.hs@gmail.com>
2021-08-01 18:03:59 +0300

    fixed route

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,40 +4,25 @@ import React, { Component } from 'react';
 import Login from './login';
 import Signup from './Signup';
 import LogOut from './LogOut';
-import io from 'socket.io-client';
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5000/';
-const socket = io(SERVER_URL, { transports: ['websocket'] });
 
 export class Home extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.loggedIn && <LogOut logOut={this.props.logOut} />}
 
-    componentDidMount = () => {
-
-        socket.on('connect', () => {
-            socket.emit('test');
-        })
-      }
-    
-    
-    render() {
-        return (
-            <div>
-                {this.props.loggedIn &&
-                    <LogOut logOut={this.props.logOut} />}
-
-                {!this.props.loggedIn &&
-                    <>
-                        <Login loggedIn={this.props.loggedIn}
-                            loggedInFunction={this.props.loggedInFunction}
-                        />
-                        <Signup />
-                    </>
-
-                }
-                {/* {this.state.loggedIn && <Redirect to="/main" />} */}
-
-            </div>
-        )
-    }
+        {!this.props.loggedIn && (
+          <>
+            <Login
+              loggedIn={this.props.loggedIn}
+              loggedInFunction={this.props.loggedInFunction}
+            />
+            <Signup />
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
