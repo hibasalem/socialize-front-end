51120cee5d45438f26503d8d073d76ac454e1cdf
yazantafesh <yazantafesh1@gmail.com>
2021-08-02 17:39:28 +0300

    messages and groups

--- src/components/AddFriends.js
+++ src/components/AddFriends.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Friend from './Friend';
 
 export class AddFriends extends Component {
+  
   render() {
     return (
+      
       <div>
         {this.props.allusers.map((item, idx) => {
+
           return (
             <Friend
               key={idx}
               item={item}
-              handleAddFriend={this.props.handleAddFriend}
+              handleAddFriend={this.props.handleAddFriend}              
             />
           );
         })}
