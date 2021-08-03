fc9394d41eba7976556bec35d228d44c4ba89bfc
yazantafesh <yazantafesh1@gmail.com>
2021-08-04 01:17:25 +0300

    done with all groups stuff

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 
 export class CommentForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            content:'',
+        this.state = {
+            content: '',
         }
-    }   
-    comment=(e)=>{
+    }
+    comment = (e) => {
         e.preventDefault();
-        this.props.comment(this.state.content,this.props.id);
+        this.props.comment(this.state.content, this.props.id);
     }
     render() {
         return (
             <div>
-                    <form onSubmit={(e)=>{this.comment(e)}} >
+                <form onSubmit={(e) => { this.comment(e) }} >
                     <label>comment</label>
-                    <input type='text' onChange={(e)=>{
+                    <input type='text' onChange={(e) => {
                         this.setState({
-                            content:e.target.value
+                            content: e.target.value
                         })
                     }} />
                     <input type='submit' />
